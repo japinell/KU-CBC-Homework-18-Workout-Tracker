2e99fd28bc273d91c7baaf93d089fe0d81dc9535
JAPinell <japinell@yahoo.com>
2021-07-21 22:34:27 -0500

    Fixed Workout schema

--- models/Workout.js
+++ models/Workout.js
@@ -12,6 +12,7 @@ const workoutSchema = new Schema({
       type: { type: String },
       name: { type: String },
       duration: { type: Number },
+      distance: { type: Number },
       weight: { type: Number },
       reps: { type: Number },
       sets: { type: Number },
