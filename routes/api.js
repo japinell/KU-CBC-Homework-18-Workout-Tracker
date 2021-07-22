a8c4db8b8dc48994cca9b3833efc41badfe47392
JAPinell <japinell@yahoo.com>
2021-07-21 22:06:31 -0500

    Implemented adding multiple exercises on the same workout

--- routes/api.js
+++ routes/api.js
@@ -20,7 +20,7 @@ router.post("/api/workouts", ({ body }, res) => {
 // Handles the update of a single workout
 //
 router.put("/api/workouts/:id", (req, res) => {
-  Workout.update({ _id: req.params.id }, { $set: { exercises: req.body } })
+  Workout.update({ _id: req.params.id }, { $push: { exercises: req.body } })
     .then((document) => {
       res.json(document);
     })
